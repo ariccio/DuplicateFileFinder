c4eeb86474cfcc370b58e012bbe41854ebe758ff
Alexander Riccio <alexander@riccio.com>
2014-03-12 06:46:17 -0400

    Cleanup & reorganization
    Preparing for new (smarter!) algorithm

--- py_duplicate_pysummer.py
+++ py_duplicate_pysummer.py
@@ -45,30 +45,43 @@ class Worker():
         else:
             self.name = "worker-%s" % self.hashname
         #logging.debug("%s started!" % self.name)
+
     def compute(self, fname, incremental = None):
-        #[logging.debug("compute: %s" % ( str(arg))) for arg in args]
+        self.__fname = fname
         try:
             self.hashdata = hashlib.new(self.hashname)
-
         except ValueError:
             raise NotImplementedError("# %s : hash algorithm [ %s ] not implemented" % (self.name, self.hashname))
         incremental = True
+        #except KeyboardInterrupt:
+            #return
         try:
-            with open(fname, 'rb') as fhandle:
+            logging.debug('\tcompute opening %s' % ( str(self.__fname) ))
+            with open(self.__fname, 'rb') as fhandle:
                 if incremental is not None:
+                    if NO_HUMANFRIENDLY is None:
+                        logging.debug('\t\treading incrementally... (increments of: %s bytes)' % ( str(self.bufsize) ) )
+                    elif NO_HUMANFRIENDLY is not None:
+                        logging.debug('\t\treading incrementally... (increments of: %s)' % ( humanfriendly.format_size(self.bufsize) ) )
                     data = fhandle.read(self.bufsize)                
                     while(data):
                         self.hashdata.update(data)
                         data = fhandle.read(self.bufsize)
                 else:
+                    logging.debug('\t\treading in one huge chunk...')
                     data = fhandle.read()#this is ugly and will break for large files
                     self.hashdata.update(data)
         except IOError:
             logging.debug("whoops! IOError in Worker.compute")
-            return ('','')
-        
+            #return ('','')
+        except KeyboardInterrupt:
+            sys.exit()
         #outputHashQueue.put(fname, self.hashdata.hexdigest())
-        return (fname, self.hashdata.hexdigest())
+#        return (fname, self.hashdata.hexdigest())
+        logging.debug('\t\t\t\tfile: "%s" : %s' % (str(self.__fname), str(self.hashdata.hexdigest())) )
+        self.__outTuple = (self.__fname, self.hashdata.hexdigest())
+        return self.__outTuple
+        
 
 def mCompute(job_q, __hashname, out_q, incremental = None, rmode='rb', bufsize=16777216, __name=None):
     __hash_known = ['sha1', 'sha512']
@@ -112,6 +125,31 @@ def mCompute(job_q, __hashname, out_q, incremental = None, rmode='rb', bufsize=1
 
 
 def getFileSizeFromOS(theFileInQuestion):
+    '''
+    finding the filesize is a bitch. A call to os.stat("pathToSomeFile") returns (on Windows):
+
+    nt.stat_result(st_mode=33206, st_ino=0L, st_dev=0, st_nlink=0, st_uid=0, st_gid=0, st_size=6253L, st_atime=1391494554L, st_mtime=1391507902L, st_ctime=1391494554L)
+
+    Not exactly readable.
+
+    What we want is st_size, which is 6253 (bytes). This may be less useful than you think, as with NTFS filesystem level compression the filesize ON DISK can be tremendously different.
+
+    The return type of os.stat(), on Windows, is \'nt.stat_result\', so first I strify it.
+    Then, accessing the string as a list, I grab everything between [inbetween?] the parenthesis (i.e. st_mode=...st_ctime=).
+    Next, I split the remaining string with \', \' as the delimiter.
+    I then split each item in (what used to be a str) the list with \'=\' as the delimiter.
+    Now each item in the list is another (2 item) list (i.e. [[\'st_mode\', \'333206\'],...]).
+    I\'m not sure if the position of st_size is constant, so I now iterate over the list (yes, slow linear search) until I find where it is, and store that location.
+    I then store the second item at that position, i.e.:
+             this one↓↓↓↓
+    [[\'st_size\', \'6253L\'],...]
+    Note the trailing L  ↑!
+    I need to trim that trailing \'L\', else we'll run into problems later. So I store the list at [:-1]
+    Now I intify our nicely trimmed integer-only str, and we\'re done!
+
+    ...mostly - now it's time to display this information in a usuable mannner
+
+    '''
     try:
         aFileStats = str(os.stat(theFileInQuestion))
         listAFileStats = aFileStats[aFileStats.index('(')+1 : aFileStats.index(')')].split(', ')
@@ -144,6 +182,20 @@ def getFileSizeFromOS(theFileInQuestion):
         pass
     return fileSizeInBytes
 
+def printListOfDuplicateFiles(listOfDuplicateFiles):
+    if NO_HUMANFRIENDLY is not None:
+        for item in listOfDuplicateFiles:
+            item[0] = humanfriendly.format_size(item[0])
+
+    for item in listOfDuplicateFiles:
+        print("\n%s:" % (str(item[0])))
+        for aFileName in item[1]:
+            try:
+                print("\t%s" % (str(aFileName)))    
+            except UnicodeEncodeError:
+                logging.debug("\t\t\tfilename is evil! filename: ", aFileName)
+
+
 def printDuplicateFilesAndReturnWastedSpace(knownFiles):
     '''
     prints duplicate files (as of now, only those > 0 bytes) in the form of
@@ -156,98 +208,58 @@ def printDuplicateFilesAndReturnWastedSpace(knownFiles):
     f9f629771f00de09cb63f10a4dbae4d7a8f72544:
     59217687 bytes          C:\\Users\\Alexander Riccio\\Downloads\\testDir\\blah.pdf
     59217687 bytes          C:\\Users\\Alexander Riccio\\Downloads\\testDir\\blahBlah.pdf
-
-
-    finding the filesize is a bitch. A call to os.stat("pathToSomeFile") returns (on Windows):
-
-    nt.stat_result(st_mode=33206, st_ino=0L, st_dev=0, st_nlink=0, st_uid=0, st_gid=0, st_size=6253L, st_atime=1391494554L, st_mtime=1391507902L, st_ctime=1391494554L)
-
-    Not exactly readable.
-
-    What we want is st_size, which is 6253 (bytes). This may be less useful than you think, as with NTFS filesystem level compression the filesize ON DISK can be tremendously different.
-
-    The return type of os.stat(), on Windows, is \'nt.stat_result\', so first I strify it.
-    Then, accessing the string as a list, I grab everything between [inbetween?] the parenthesis (i.e. st_mode=...st_ctime=).
-    Next, I split the remaining string with \', \' as the delimiter.
-    I then split each item in (what used to be a str) the list with \'=\' as the delimiter.
-    Now each item in the list is another (2 item) list (i.e. [[\'st_mode\', \'333206\'],...]).
-    I\'m not sure if the position of st_size is constant, so I now iterate over the list (yes, slow linear search) until I find where it is, and store that location.
-    I then store the second item at that position, i.e.:
-             this one↓↓↓↓
-    [[\'st_size\', \'6253L\'],...]
-    Note the trailing L  ↑!
-    I need to trim that trailing \'L\', else we'll run into problems later. So I store the list at [:-1]
-    Now I intify our nicely trimmed integer-only str, and we\'re done!
-
-    ...mostly - now it's time to display this information in a usuable mannner
-
     
     If you have humanfriendly ("humansize") installed, the output will be more readable - i.e. 1 KB, not 1024.
     I also increment a counter, wastedSpace, by filesize over each file.
     '''
     wastedSpace = 0
     sizeOfKnownFiles = {}
+
+    logging.debug('\tcalculating wasted space...')
+    
     for key in knownFiles:
         aFile = knownFiles[key][0]
-        try:
-            aFileStats = str(os.stat(aFile))
-            listAFileStats = aFileStats[aFileStats.index('(')+1 : aFileStats.index(')')].split(', ')
+        fileSizeInBytes =  getFileSizeFromOS(aFile)
 
-            for item in listAFileStats:
-                listAFileStats[listAFileStats.index(item)] = item.split('=')
+        if len(knownFiles[key]) > 1:
+            wastedSpace += fileSizeInBytes * len(knownFiles[key])
 
-            for item in listAFileStats:
-                if item[0] == 'st_size':
-                    sizeIndex = listAFileStats.index(item)
-            fileSizeStr = listAFileStats[sizeIndex][1]
-            if fileSizeStr[-1] == 'L':
-                fileSizeInBytes = fileSizeStr[:-1]
-            else:
-                fileSizeInBytes = fileSizeStr
-            try:
-                fileSizeInBytes = int(fileSizeInBytes)
-            except ValueError:#occurs when st_size == ''
-                logging.debug("Whoops! ValueError in printDuplicateFilesAndReturnWastedSpace! This usually occurs when st_size == \'\'")
-                logging.debug("\taFileStats       = %s" % aFileStats)
-                logging.debug("\tlistAFileStats   = %s" % (str(listAFileStats)))
-                logging.debug("\tsizeIndex        = %s" % (str(sizeIndex)))
-                logging.debug("\tfileSizeStr      = %s" % fileSizeStr)
-                logging.debug("\tfileSizeStr[:-1] = %s" % (str(fileSizeStr[:-1])))
-                logging.debug("\tfileSizeInBytes:")
-                logging.debug(fileSizeInBytes)
-                fileSizeInBytes = 0
-                
-            if len(knownFiles[key]) > 1:
-                wastedSpace += fileSizeInBytes * len(knownFiles[key])
-                if fileSizeInBytes > 0:
-                    sizeOfKnownFiles[key] = fileSizeInBytes * len(knownFiles[key])
-                    logging.debug("\n%s:"%key)
-                    for aSingleFile in knownFiles[key]:
-                        if NO_HUMANFRIENDLY is None:
-                            logging.debug("%s bytes\t\t%s" % (fileSizeInBytes, aSingleFile))
-                        elif NO_HUMANFRIENDLY is not None:
-                            logging.debug("%s\t\t%s" % (humanfriendly.format_size(fileSizeInBytes), aSingleFile))
-        except WindowsError:
-            logging.debug("Windows error in printDuplicateFilesAndReturnWastedSpace! May god have mercy on your soul.")
-            pass
+            if fileSizeInBytes > 0:
+                sizeOfKnownFiles[key] = fileSizeInBytes * len(knownFiles[key])
+                logging.debug("\n\t\t\t%s:"%key)
+
+                for aSingleFile in knownFiles[key]:
+
+                    if NO_HUMANFRIENDLY is None:
+                        logging.debug("\t\t\t%s bytes\t\t\t%s" % (fileSizeInBytes, aSingleFile))
+                    elif NO_HUMANFRIENDLY is not None:
+                        logging.debug("\t\t\t%s\t\t\t%s" % (humanfriendly.format_size(fileSizeInBytes), aSingleFile))
+        else:
+            logging.debug('\tlen(knownFiles[key]) IS NOT greater than 1')
+            
     sortedSizeOfKnownFiles = sorted(sizeOfKnownFiles, key=knownFiles.__getitem__)
     sortedListSize = []
+    
     for sortedHash in sortedSizeOfKnownFiles:
         sortedListSize.append([sizeOfKnownFiles.get(sortedHash), knownFiles.get(sortedHash)])
 
     sortedListSize.sort()
-    if NO_HUMANFRIENDLY is not None:
-        for item in sortedListSize:
-            item[0] = humanfriendly.format_size(item[0])
-    for item in sortedListSize:
-        print("\n%s:" % (str(item[0])))
-        for aFileName in item[1]:
-            try:
-                print("\t%s" % (str(aFileName)))    
-            except UnicodeEncodeError:
-                logging.debug("filename is evil! filename: ", aFileName)
-                
-    
+
+    printListOfDuplicateFiles(sortedListSize)
+
+##    if NO_HUMANFRIENDLY is not None:
+##        for item in sortedListSize:
+##            item[0] = humanfriendly.format_size(item[0])
+##
+##    for item in sortedListSize:
+##        print("\n%s:" % (str(item[0])))
+##        for aFileName in item[1]:
+##            try:
+##                print("\t%s" % (str(aFileName)))    
+##            except UnicodeEncodeError:
+##                logging.debug("\t\t\tfilename is evil! filename: ", aFileName)
+##                
+##    
     return wastedSpace#, sortedSizeOfKnownFiles
 
 def fInit(q):
@@ -266,76 +278,83 @@ def main():
     if args:
         arg0 = args[0]
         knownFiles = {}
-        if True:
-            if options.hashname == "auto":
-                options.hashname = "sha1"
-                logging.warning("'auto' as hash selected, so defaulting to 'sha1'")
+        if options.hashname == "auto":
+            options.hashname = "sha1"
+            logging.warning("'auto' as hash selected, so defaulting to 'sha1'")
 
-            w = Worker(options.hashname)
+        w = Worker(options.hashname)
 
-            if os.path.isfile(arg0):
-                hw = w.compute(arg0)
-                print("%s *%s" % (hw, arg0))
-                knownFiles[hw] = arg0
+        if os.path.isfile(arg0):
+            hw = w.compute(arg0)
+            print("%s *%s" % (hw, arg0))
+            knownFiles[hw] = arg0
 
-            elif os.path.isdir(arg0):
-                pathsToFilesToBeHashed = []
-                for root, dirs, files in os.walk(arg0):
-                    '''move to:
-                        build queue of files -> build pool of processes -> start processes
-                        like I did in factah over summer, where mp_factorizer is passed nums(a list of numbers to factorize) and a number nprocs (how many processes)
+        elif os.path.isdir(arg0):
+            pathsToFilesToBeHashed = []
+            logging.debug('Walking %s...' % ( str(arg0) ) )
+            for root, dirs, files in os.walk(arg0):
+                '''move to:
+                    build queue of files -> build pool of processes -> start processes
+                    like I did in factah over summer, where mp_factorizer is passed nums(a list of numbers to factorize) and a number nprocs (how many processes)
 
-                    '''
+                '''
                     
-                    #queueOfFileHashes      = multiprocessing.Queue()
-                job_q = multiprocessing.Queue()    
+                    #######queueOfFileHashes      = multiprocessing.Queue()
+                job_q = queue.Queue()
                 for fname in files:
-                    
                     fullpath = os.path.abspath(os.path.join(root, fname))
-                    #logging.debug("putting %s in queueOfPathsToFilesToBeHashed"% (str(fullpath))) 
+                    logging.debug("\tPutting %s in queueOfPathsToFilesToBeHashed"% (str(fullpath))) 
                     job_q.put(fullpath)
-                    
-                    #hw = w.compute(fullpath)
-                fileHashes             = []
-                # Each process will get 'chunksize' nums and a queue to put his out dict into
-                out_q = multiprocessing.Queue()
-                #worker(nums, out_q)
-                procs = []
-                nProcesses = 8
-                
-                for i in range(nProcesses):
-                    logging.debug("starting %i...\tprocs = %s" % (i, str(procs)))
-                    p = multiprocessing.Process( target=mCompute, args=(job_q, 'sha1', out_q, i))
-                    #fname, hashname, incremental = None, rmode='rb', bufsize=16777216, name=None
-                    procs.append(p)
-                    p.start()
-                logging.debug('All started! Waiting.....')
+                job_q.put(False)
+                #hw = w.compute(fullpath)
+            fileHashes = []
+            # Each process will get 'chunksize' nums and a queue to put his out dict into
+            out_q = queue.Queue()
+            procs = []
+            nProcesses = 8
+            
+            
+            #logging.debug('All started! Waiting.....')
+            aWorker = Worker(options.hashname)
+            logging.debug('Starting computation!')
+            try:
+                thisHashFileName = job_q.get()
+                while thisHashFileName:
+                    result = aWorker.compute(thisHashFileName)
+                    out_q.put(result)
+                    thisHashFileName = job_q.get()
+                out_q.put(False)                
+                #fileHashes = []
 
-                for p in procs:
-                    p.join()
                 nextHash = out_q.get()
                 while(nextHash):
                     fileHashes.append(nextHash)
                     nextHash = out_q.get()
-                try:
-                    for (fileFullPath, fileHashHex) in fileHashes:
-                        
-                        it = knownFiles.get(fileHashHex)
-                        if it is None:
-                            knownFiles[fileHashHex] = [fileFullPath]
-                        else:
-                            knownFiles[fileHashHex].append(fileFullPath)
-                        #(fileFullPath, fileHashHex)  = queueOfFileHashes.get()
-                except ValueError:
-                    logging.debug(fileFullPath, fileHashHex)
-            else:
-                raise IOError("Specified file or directory not found")
+
+            except KeyboardInterrupt:
+                sys.exit()
+            
+            for (fileFullPath, fileHashHex) in fileHashes:
+                try:                    
+                    it = knownFiles.get(fileHashHex)
+                    if it is None:
+                        knownFiles[fileHashHex] = [fileFullPath]
+                    else:
+                        knownFiles[fileHashHex].append(fileFullPath)
+                    #(fileFullPath, fileHashHex)  = queueOfFileHashes.get()
+                except KeyboardInterrupt:
+                    sys.exit()
+        else:
+            raise IOError("Specified file or directory not found")
+
         wastedSpace= printDuplicateFilesAndReturnWastedSpace(knownFiles)
-        print('\n\n')
+        print('\n')
         if NO_HUMANFRIENDLY is None:
             print("%s bytes of wasted space!" % wastedSpace)
         elif NO_HUMANFRIENDLY is not None:
             print("%s of wasted space!" % humanfriendly.format_size(wastedSpace))           
-        #print(sortedSizeOfKnownFiles)
+
+
+
 if __name__ == "__main__":
     main()
